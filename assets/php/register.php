8c68a85c6ecf4ea9ec4d6f870a6c300950bc218d
Dilan Steven Varela <dilans.varela@gmail.com>
2022-04-28 15:28:00 -0500

    Last version

--- assets/php/register.php
+++ assets/php/register.php
@@ -2,6 +2,6 @@
 $post_data = $_POST;
 session_start();
 //Create session for display success message
-$_SESSION['success'] = "$post_data[full_name], has sido inscrito exitosamente en el Hackathon Taxis Libres 2022";
+$_SESSION['success'] = "$post_data[full_name], has sido inscrito exitosamente en el Hackathon Taxis Libres 2022.";
 //Return with focus in register 
 header("Location: https://hackathon-2022.herokuapp.com/#registro-al-evento");exit();
